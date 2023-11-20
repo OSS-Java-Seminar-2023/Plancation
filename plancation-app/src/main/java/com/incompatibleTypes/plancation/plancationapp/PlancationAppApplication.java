50065583f5da15bf24136d56bf48a1496fd03b4e
mario miletić <mariomiletic22@gmail.com>
2023-11-20 14:42:07 +0100

    added Spring Boot project using spring initializr

---
+++ plancation-app/src/main/java/com/incompatibleTypes/plancation/plancationapp/PlancationAppApplication.java
@@ -0,0 +1,13 @@
+package com.incompatibleTypes.plancation.plancationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlancationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlancationAppApplication.class, args);
+	}
+
+}
---
+++ plancation-app/src/test/java/com/incompatibleTypes/plancation/plancationapp/PlancationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incompatibleTypes.plancation.plancationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlancationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
