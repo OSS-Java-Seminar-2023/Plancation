d46e319fd5cced547afb312c419b2f418f72d4df
Mario Miletić <92264175+m-miletic@users.noreply.github.com>
2023-11-20 15:29:26 +0100

    Update migr-script.sql

--- migr-script.sql
+++ migr-script.sql
@@ -1,83 +1,61 @@
-CREATE TABLE `employee` (
-  `employee_id` long PRIMARY KEY AUTO_INCREMENT,
-  `employee_role` integer,
-  `employee_username` varchar(255),
-  `employee_department` integer,
-  `employee_request` integer,
-  `employee_file` integer,
-  `employee_total_vacation_days` integer,
-  `employee_bonus_vacation_days` integer
+CREATE TABLE employee (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  username varchar(255),
+  mail varchar(255),
+  department_id integer,
+  request_id integer,
+  minimal_vacation_day_id integer,
+  total_vacation_days integer,
+  bonus_vacation_day_id integer,
+  preceding_year_vacation_days integer,
+  date_employed timestamp
+);
+CREATE TABLE department (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  name varchar(255),
+  manager_id integer
 );
 
-CREATE TABLE `department` (
-  `department_id` long PRIMARY KEY AUTO_INCREMENT,
-  `department_name` varchar(255),
-  `department_manager` integer,
-  `department_vacation_days_num` integer
+CREATE TABLE request (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  file_id varchar(255),
+  request_time timestamp
 );
 
-CREATE TABLE `request` (
-  `request_id` long PRIMARY KEY AUTO_INCREMENT,
-  `request_type` integer,
-  `request_file_path` varchar(255),
-  `request_status` enum,
-  `request_time_published` timestamp,
-  `request_published_by` varchar(255),
-  `request_approved_by` varchar(255)
+CREATE TABLE bonus_vacation_day (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  description varchar(255),
+  number_of_days integer
 );
 
-CREATE TABLE `request_type` (
-  `request_type_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `request_type_description` enum
+CREATE TABLE file (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  title varchar(255),
+  path varchar(255)
 );
 
-CREATE TABLE `bonus_vacation_day` (
-  `bonus_vacation_day_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `bonus_vacation_day_name` enum
+CREATE TABLE minimal_vacation_day (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  description varchar(255),
+  number_of_days integer
 );
 
-CREATE TABLE `state_holiday` (
-  `state_holiday_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `state_holiday_name` enum
+CREATE TABLE employee_bonus_vacation_day (
+  employee_bonus_vacation_day_id integer,
+  bonus_vacation_day_id integer,
+  PRIMARY KEY (employee_bonus_vacation_day_id, bonus_vacation_day_id)
 );
 
-CREATE TABLE `role` (
-  `role_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `role_name` enum
-);
+ALTER TABLE employee ADD FOREIGN KEY (department_id) REFERENCES department (id);
 
-CREATE TABLE `request_status` (
-  `request_status_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `request_status_description` enum
-);
+ALTER TABLE employee ADD FOREIGN KEY (request_id) REFERENCES request (id);
 
-CREATE TABLE `file` (
-  `file_id` integer PRIMARY KEY AUTO_INCREMENT,
-  `file_title` varchar(255),
-  `file_path` varchar(255)
-);
+ALTER TABLE employee ADD FOREIGN KEY (minimal_vacation_day_id) REFERENCES minimal_vacation_day (id);
 
-ALTER TABLE `employee` ADD FOREIGN KEY (`employee_department`) REFERENCES `department` (`department_id`);
+ALTER TABLE department ADD FOREIGN KEY (manager_id) REFERENCES employee (id);
 
-ALTER TABLE `employee` ADD FOREIGN KEY (`employee_role`) REFERENCES `role` (`role_id`);
+ALTER TABLE request ADD FOREIGN KEY (file_id) REFERENCES file (id);
 
-CREATE TABLE `employee_bonus_vacation_day` (
-  `employee_employee_bonus_vacation_days` integer,
-  `bonus_vacation_day_bonus_vacation_day_id` integer,
-  PRIMARY KEY (`employee_employee_bonus_vacation_days`, `bonus_vacation_day_bonus_vacation_day_id`)
-);
+ALTER TABLE employee_bonus_vacation_day ADD FOREIGN KEY (employee_bonus_vacation_day_id) REFERENCES employee (id);
 
-ALTER TABLE `employee_bonus_vacation_day` ADD FOREIGN KEY (`employee_employee_bonus_vacation_days`) REFERENCES `employee` (`employee_bonus_vacation_days`);
-
-ALTER TABLE `employee_bonus_vacation_day` ADD FOREIGN KEY (`bonus_vacation_day_bonus_vacation_day_id`) REFERENCES `bonus_vacation_day` (`bonus_vacation_day_id`);
-
-
-ALTER TABLE `request` ADD FOREIGN KEY (`request_id`) REFERENCES `employee` (`employee_request`);
-
-ALTER TABLE `request` ADD FOREIGN KEY (`request_type`) REFERENCES `request_type` (`request_type_id`);
-
-ALTER TABLE `request` ADD FOREIGN KEY (`request_status`) REFERENCES `request_status` (`request_status_id`);
-
-ALTER TABLE `file` ADD FOREIGN KEY (`file_id`) REFERENCES `employee` (`employee_file`);
-
-ALTER TABLE `file` ADD FOREIGN KEY (`file_id`) REFERENCES `request` (`request_file_path`);
+ALTER TABLE employee_bonus_vacation_day ADD FOREIGN KEY (bonus_vacation_day_id) REFERENCES bonus_vacation_day (id);
